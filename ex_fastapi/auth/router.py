c56d150c5698a6fb6b06d2170b787c596a7ec473
alta7700 <alta7700@mail.ru>
2023-02-21 00:33:51 +0300

    и ещё чуть-чуть

--- ex_fastapi/auth/router.py
+++ ex_fastapi/auth/router.py
@@ -74,6 +74,9 @@ def create_auth_router(
 
 def create_users_router(
         auth_provider: "AuthProvider" = None,
+        registration_route: bool = True,
+        activate_account_route: bool = True,
+        edit_me_route: bool = True,
         **kwargs
 ) -> CRUDRouter:
 
@@ -90,72 +93,75 @@ def create_users_router(
 
     router = CRUDRouter(service=service, complete_auto_routes=False, **kwargs)
 
-    @router.post('/registration', status_code=201, responses=Codes.responses(
-        (Codes.activation_email, {'uuid': uuid4()}),
-        (router.not_unique_error_instance(), {'fields': ['поле1', 'поле2']})
-    ))
-    async def registration(
-            background_tasks: BackgroundTasks,
-            data: get_schema(UserRegistration) = Body(...)
-    ):
-        try:
-            user = await router.service.create(data, background_tasks=background_tasks)
-        except NotUnique as e:
-            raise router.not_unique_error(e.fields)
-        user_repo = UserRepository(user)
-        await user_repo.post_registration(background_tasks)
-        return Codes.activation_email.resp_detail(uuid=user_repo.uuid)
+    if registration_route:
+        @router.post('/registration', status_code=201, responses=Codes.responses(
+            (Codes.activation_email, {'uuid': uuid4()}),
+            (router.not_unique_error_instance(), {'fields': ['поле1', 'поле2']})
+        ))
+        async def registration(
+                background_tasks: BackgroundTasks,
+                data: get_schema(UserRegistration) = Body(...)
+        ):
+            try:
+                user = await router.service.create(data, background_tasks=background_tasks)
+            except NotUnique as e:
+                raise router.not_unique_error(e.fields)
+            user_repo = UserRepository(user)
+            await user_repo.post_registration(background_tasks)
+            return Codes.activation_email.resp_detail(uuid=user_repo.uuid)
 
-    @router.get('/activation', response_model=UserMeRead, responses=Codes.responses(
-        router.not_found_error_instance(),
-        Codes.activation_email_resend,
-        Codes.activation_email_code_incorrect,
-        Codes.already_active,
-    ))
-    async def activate_account(
-            background_tasks: BackgroundTasks,
-            response: Response,
-            uuid: UUID = Query(...),
-            code: str = Query(min_length=6, max_length=6)
-    ):
-        # TODO: избавиться от select_related и prefetch_related, сделать какой-то может опять глобальный объект или
-        #  типа того
-        user = await UserRepository.get_user_by('uuid', uuid)\
-            .select_related('temp_code').prefetch_related('permissions', 'groups__permissions')
-        if not user:
-            raise router.not_found_error()
-        user_repo = UserRepository(user)
-        if user_repo.is_user_active:
-            raise Codes.already_active.err()
-        temp_code_error = user_repo.check_temp_code_error(code)
-        if temp_code_error:
-            if temp_code_error == 'expired':
-                user_repo.add_send_activation_email_task(background_tasks=background_tasks)
-                raise Codes.activation_email_resend.err(background=background_tasks)
-            if temp_code_error == 'incorrect':
-                raise Codes.activation_email_code_incorrect.err()
-        await user_repo.activate()
-        # TODO: заменить на что-то поуниверсальнее, а не только куки
-        auth_provider.set_auth_cookie(response, user_repo.user)
-        return UserMeRead.from_orm(user_repo.user)
+    if activate_account_route:
+        @router.get('/activation', response_model=UserMeRead, responses=Codes.responses(
+            router.not_found_error_instance(),
+            Codes.activation_email_resend,
+            Codes.activation_email_code_incorrect,
+            Codes.already_active,
+        ))
+        async def activate_account(
+                background_tasks: BackgroundTasks,
+                response: Response,
+                uuid: UUID = Query(...),
+                code: str = Query(min_length=6, max_length=6)
+        ):
+            # TODO: избавиться от select_related и prefetch_related, сделать какой-то может опять глобальный объект или
+            #  типа того
+            user = await UserRepository.get_user_by('uuid', uuid)\
+                .select_related('temp_code').prefetch_related('permissions', 'groups__permissions')
+            if not user:
+                raise router.not_found_error()
+            user_repo = UserRepository(user)
+            if user_repo.is_user_active:
+                raise Codes.already_active.err()
+            temp_code_error = user_repo.check_temp_code_error(code)
+            if temp_code_error:
+                if temp_code_error == 'expired':
+                    user_repo.add_send_activation_email_task(background_tasks=background_tasks)
+                    raise Codes.activation_email_resend.err(background=background_tasks)
+                if temp_code_error == 'incorrect':
+                    raise Codes.activation_email_code_incorrect.err()
+            await user_repo.activate()
+            # TODO: заменить на что-то поуниверсальнее, а не только куки
+            auth_provider.set_auth_cookie(response, user_repo.user)
+            return UserMeRead.from_orm(user_repo.user)
 
-    @router.patch('/me', response_model=UserMeRead, responses=AuthErrors.responses(
-        *AuthErrors.all_errors(),
-        router.not_found_error_instance(),
-        (router.not_unique_error_instance(), {'fields': ['поле1', 'поле2']})
-    ))
-    async def edit_me(
-            background_tasks: BackgroundTasks,
-            user_repo: UserRepository = Depends(dependencies.user_with_perms()),
-            data: get_schema(UserMeEdit) = Body(...),
-    ):
-        try:
-            item = await router.service.edit(user_repo.user, data, background_tasks=background_tasks)
-        except ItemNotFound:
-            raise router.not_found_error()
-        except NotUnique as e:
-            raise router.not_unique_error(e.fields)
-        return UserMeRead.from_orm(item)
+    if edit_me_route:
+        @router.patch('/me', response_model=UserMeRead, responses=AuthErrors.responses(
+            *AuthErrors.all_errors(),
+            router.not_found_error_instance(),
+            (router.not_unique_error_instance(), {'fields': ['поле1', 'поле2']})
+        ))
+        async def edit_me(
+                background_tasks: BackgroundTasks,
+                user_repo: UserRepository = Depends(dependencies.user_with_perms()),
+                data: get_schema(UserMeEdit) = Body(...),
+        ):
+            try:
+                item = await router.service.edit(user_repo.user, data, background_tasks=background_tasks)
+            except ItemNotFound:
+                raise router.not_found_error()
+            except NotUnique as e:
+                raise router.not_unique_error(e.fields)
+            return UserMeRead.from_orm(item)
 
     router.complete_auto_routes()
     return router
